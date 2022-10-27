70d17488509ff86f17f910c75e2b1ec83e82b5b9
cbaucom <chrisrb83@gmail.com>
2022-10-27 01:11:56 -0400

    Add button to trigger modal and set up initial modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import {
+  Button,
   chakra,
   Flex,
+  FormControl,
+  FormLabel,
   Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
   Table,
   Tbody,
   Td,
   Th,
   Thead,
   Tr,
+  useDisclosure,
 } from '@chakra-ui/react'
 import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useTable, useSortBy } from 'react-table'
 
 import { Container } from '../components/Container'
@@ -41,8 +53,23 @@ export async function getStaticProps() {
   }
 }
 
-const Index = ({ listOfShifts }) => {
+const Index = ({ nurses, listOfShifts }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const nurseSelectField = useRef({} as HTMLSelectElement)
+  const shiftSelectField = useRef({} as HTMLSelectElement)
+  const submitButtonRef = useRef({} as HTMLButtonElement)
   const [shifts, setShifts] = useState(listOfShifts)
+  const [selectedNurse, setSelectedNurse] = useState('')
+  const [selectedShift, setSelectedShift] = useState('')
+
+  console.log(
+    '🚀 ~ file: index.tsx ~ line 63 ~ Index ~ selectedNurse',
+    selectedNurse
+  )
+  console.log(
+    '🚀 ~ file: index.tsx ~ line 65 ~ Index ~ selectedShift',
+    selectedShift
+  )
 
   useEffect(() => {
     setShifts(listOfShifts)
@@ -85,6 +112,9 @@ const Index = ({ listOfShifts }) => {
       <Main>
         <Flex align={'center'} justify={'space-between'}>
           <Heading p={6}>Shifts</Heading>
+          <Button colorScheme="blue" onClick={onOpen}>
+            Set Shift Assignment
+          </Button>
         </Flex>
         <Table {...getTableProps()} mt={4} wordBreak={'break-all'}>
           <Thead>
@@ -132,6 +162,73 @@ const Index = ({ listOfShifts }) => {
           </Tbody>
         </Table>
       </Main>
+      <Modal onClose={onClose} isOpen={isOpen} isCentered>
+        <ModalOverlay />
+        <ModalContent
+          as={'form'}
+          maxWidth={'xl'}
+          onSubmit={(e) => {
+            e.preventDefault()
+            setSelectedNurse(nurseSelectField.current.value)
+            setSelectedShift(shiftSelectField.current.value)
+          }}
+        >
+          <ModalHeader>Set Shift Assignment</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl my={4}>
+              <FormLabel htmlFor="shift">Shift</FormLabel>
+              <Select
+                id="shift"
+                ref={shiftSelectField}
+                placeholder="Select shift"
+                size="lg"
+                isRequired
+              >
+                {shifts.map((shift) => {
+                  const shiftStartTime = shift.start.substring(
+                    shift.start.indexOf(' ') + 1
+                  )
+                  const shiftEndTime = shift.end.substring(
+                    shift.end.indexOf(' ') + 1
+                  )
+                  return (
+                    <option key={shift.id} value={shift.id}>
+                      {`${shift.name}: ${shiftStartTime} - ${shiftEndTime} (${shift.qual_required})`}
+                    </option>
+                  )
+                })}
+              </Select>
+            </FormControl>
+            <FormControl my={4}>
+              <FormLabel htmlFor="nurse">Nurse</FormLabel>
+              <Select
+                id="nurse"
+                ref={nurseSelectField}
+                placeholder="Select nurse"
+                size="lg"
+                isRequired
+              >
+                {nurses.map((nurse) => (
+                  <option key={nurse.id} value={nurse.id}>
+                    {`${nurse.first_name} ${nurse.last_name}, ${nurse.qualification}`}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter justifyContent={'flex-start'}>
+            <Button
+              ref={submitButtonRef}
+              colorScheme="blue"
+              textTransform="uppercase"
+              type="submit"
+            >
+              Save Assignment
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Container>
   )
 }
