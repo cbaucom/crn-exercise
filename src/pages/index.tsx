fea382becdfba3fb6d68562ef81356ec16cf6b93
cbaucom <chrisrb83@gmail.com>
2022-10-27 01:22:47 -0400

    Remaining logic for saving shifts, warning if unavailable or unqualified

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,18 +16,29 @@ import {
   Table,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
   useDisclosure,
 } from '@chakra-ui/react'
-import { TriangleDownIcon, TriangleUpIcon } from '@chakra-ui/icons'
+import {
+  TriangleDownIcon,
+  TriangleUpIcon,
+  WarningTwoIcon,
+} from '@chakra-ui/icons'
 import React, { useEffect, useRef, useState } from 'react'
 import { useTable, useSortBy } from 'react-table'
 
 import { Container } from '../components/Container'
 import { Main } from '../components/Main'
-import { getNurses, getShifts } from '../lib/rn_api'
+import {
+  assignShift,
+  getNurses,
+  getShifts,
+  isNurseAvailable,
+  isNurseQualified,
+} from '../lib/rn_api'
 
 export async function getStaticProps() {
   const nurses = await getNurses()
@@ -58,22 +69,103 @@ const Index = ({ nurses, listOfShifts }) => {
   const nurseSelectField = useRef({} as HTMLSelectElement)
   const shiftSelectField = useRef({} as HTMLSelectElement)
   const submitButtonRef = useRef({} as HTMLButtonElement)
+  const availabilityErrorRef = useRef({} as HTMLDivElement)
+  const qualifiedErrorRef = useRef({} as HTMLDivElement)
   const [shifts, setShifts] = useState(listOfShifts)
   const [selectedNurse, setSelectedNurse] = useState('')
   const [selectedShift, setSelectedShift] = useState('')
 
-  console.log(
-    '🚀 ~ file: index.tsx ~ line 63 ~ Index ~ selectedNurse',
-    selectedNurse
-  )
-  console.log(
-    '🚀 ~ file: index.tsx ~ line 65 ~ Index ~ selectedShift',
-    selectedShift
-  )
+  const onSaveShiftAssignment = () => {
+    if (!selectedNurse && !selectedShift) {
+      return
+    }
+    // Find the shift that was selected
+    const shift = shifts.find((shift) => {
+      return shift.id === Number(selectedShift)
+    })
+
+    // Find the nurse that was selected
+    const nurse = nurses.find((nurse) => {
+      return nurse.id === Number(selectedNurse)
+    })
+
+    // Update the shift with the nurse's name on the server
+    assignShift(shift, nurse)
+
+    // Update the shift with the nurse's name locally
+    shift.nurse_name =
+      nurse.first_name + ' ' + nurse.last_name + ', ' + nurse.qualification
+
+    // Update the nurse_id on the shift locally
+    shift.nurse_id = nurse.id
+
+    // Update the state
+    setShifts([...shifts])
+
+    // Close the modal
+    onClose()
+  }
+
+  let enableSaveShiftAssignment = false
+
+  const testSaveShiftAssignmentEnabled = () => {
+    let enableButton = false
+
+    if (
+      typeof nurseSelectField.current.value === 'string' &&
+      nurseSelectField.current.value.length > 0 &&
+      typeof shiftSelectField.current.value === 'string' &&
+      shiftSelectField.current.value.length > 0
+    ) {
+      // Find the shift
+      const shift = shifts.find((shift) => {
+        return shift.id === Number(shiftSelectField.current.value)
+      })
+      // Find the nurse
+      const nurse = nurses.find((nurse) => {
+        return nurse.id === Number(nurseSelectField.current.value)
+      })
+
+      const isAvailable = isNurseAvailable(
+        nurse.id,
+        shift.start,
+        shift.end,
+        shifts
+      )
+      const isQualified = isNurseQualified(nurse, shift.qual_required)
+
+      if (!isAvailable && !isQualified) {
+        availabilityErrorRef.current.style.display = 'flex'
+        qualifiedErrorRef.current.style.display = 'flex'
+        enableButton = false
+      } else if (!isAvailable) {
+        availabilityErrorRef.current.style.display = 'flex'
+        qualifiedErrorRef.current.style.display = 'none'
+      } else if (!isQualified) {
+        qualifiedErrorRef.current.style.display = 'flex'
+        availabilityErrorRef.current.style.display = 'none'
+      } else {
+        availabilityErrorRef.current.style.display = 'none'
+        qualifiedErrorRef.current.style.display = 'none'
+        enableButton = true
+      }
+    }
+    if (enableButton) {
+      if (enableSaveShiftAssignment === false) {
+        enableSaveShiftAssignment = true
+        submitButtonRef.current.disabled = false
+      }
+    } else {
+      if (enableSaveShiftAssignment === true) {
+        enableSaveShiftAssignment = false
+        submitButtonRef.current.disabled = true
+      }
+    }
+  }
 
   useEffect(() => {
-    setShifts(listOfShifts)
-  }, [listOfShifts])
+    onSaveShiftAssignment()
+  }, [selectedNurse, selectedShift])
 
   // Table data and columns
   const data = shifts
@@ -184,6 +276,7 @@ const Index = ({ nurses, listOfShifts }) => {
                 placeholder="Select shift"
                 size="lg"
                 isRequired
+                onChange={() => testSaveShiftAssignmentEnabled()}
               >
                 {shifts.map((shift) => {
                   const shiftStartTime = shift.start.substring(
@@ -208,6 +301,7 @@ const Index = ({ nurses, listOfShifts }) => {
                 placeholder="Select nurse"
                 size="lg"
                 isRequired
+                onChange={() => testSaveShiftAssignmentEnabled()}
               >
                 {nurses.map((nurse) => (
                   <option key={nurse.id} value={nurse.id}>
@@ -216,12 +310,41 @@ const Index = ({ nurses, listOfShifts }) => {
                 ))}
               </Select>
             </FormControl>
+            <Flex
+              ref={qualifiedErrorRef}
+              align="center"
+              display={'none'}
+              p={4}
+              bg="antiquewhite"
+              color="maroon"
+              my={4}
+            >
+              <WarningTwoIcon />
+              <Text ml={3}>
+                This nurse is not qualified to work the chosen shift.
+              </Text>
+            </Flex>
+            <Flex
+              ref={availabilityErrorRef}
+              align="center"
+              display={'none'}
+              p={4}
+              bg="antiquewhite"
+              color="maroon"
+              my={4}
+            >
+              <WarningTwoIcon />
+              <Text ml={3}>
+                This nurse is already working during the chosen shift.
+              </Text>
+            </Flex>
           </ModalBody>
           <ModalFooter justifyContent={'flex-start'}>
             <Button
               ref={submitButtonRef}
               colorScheme="blue"
               textTransform="uppercase"
+              disabled={!enableSaveShiftAssignment}
               type="submit"
             >
               Save Assignment
