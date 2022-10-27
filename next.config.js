6adb799287431d68ca3a321f1b1fa98f34258ca2
cbaucom <chrisrb83@gmail.com>
2022-10-27 01:21:40 -0400

    Add a rewrite config to handle requests to :9001 server

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
-module.exports = {}
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/shifts/:shiftID',
+        destination: 'http://localhost:9001/shifts/:shiftID',
+      },
+    ]
+  },
+}
